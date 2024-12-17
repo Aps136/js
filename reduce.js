96515f8cc88472063dc8480e2c0920dc42b0fc18
Aps136 <aparnakaranth654@gmail.com>
2024-12-17 14:01:02 +0530

    Create reduce.js

---
+++ reduce.js
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body> 
+    <script>
+       const ar1= [1,2,3,4];
+       const init=0;
+       const sum = ar1.reduce((ac,cv)=> ac+cv,init);
+       
+       console.log(sum);
+    </script>
+    
+</body>
+</html>
