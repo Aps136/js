889878e8e181a624d0aa001eec010afe7022f082
Aps136 <aparnakaranth654@gmail.com>
2024-12-17 13:42:48 +0530

    Create map.js

---
+++ map.js
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body> 
+    <script>
+        let nums= [1,2,3,4];
+        let newar= nums.map((val)=>{
+            return val*2;
+        });
+        console.log(newar);
+    </script>
+    
+</body>
+</html>
