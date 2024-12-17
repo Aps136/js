2bc3724572cdb4d177732b17f7bd62fe6de9eda7
Aps136 <aparnakaranth654@gmail.com>
2024-12-17 13:46:55 +0530

    Update filter.js

--- filter.js
+++ filter.js
@@ -1,3 +1,4 @@
+#1
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -16,3 +17,10 @@
     
 </body>
 </html>
+#2
+const numbers = [1, 2, 3, 4, 5];
+const evens = numbers.filter(num => num % 2 === 0);
+
+console.log(evens); // Output: [2, 4]
+console.log(numbers); 
+
