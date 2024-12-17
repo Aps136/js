67e3db2608f16d727657a9754fa2b254891b00b3
Aps136 <aparnakaranth654@gmail.com>
2024-12-17 13:40:03 +0530

    Create filter.js

---
+++ filter.js
@@ -0,0 +1,18 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body> 
+    <script>
+        let ar=[1,2,3,4,5,6,7];
+        let evenar = ar.filter((val)=>{
+            return val%2===0;
+        });
+        console.log(evenar);
+      
+    </script>
+    
+</body>
+</html>
