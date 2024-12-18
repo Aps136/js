e76c2014a6da1acf3c7eda4a87ad64470f915485
Aps136 <aparnakaranth654@gmail.com>
2024-12-18 15:45:37 +0530

    Create eventobj.js

---
+++ eventobj.js
@@ -0,0 +1,10 @@
+let btn1 =document.querySelector("#btn1");
+btn1.onclick=(e)=>{
+    console.log(e);
+    
+}
+let div = document.querySelector("div");
+div.onmouseover=(e)=>{
+    console.log(e);
+
+};
