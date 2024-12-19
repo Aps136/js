71ccf3a2a076143091f0e966131781b8c643ccd9
Aps136 <aparnakaranth654@gmail.com>
2024-12-19 18:27:16 +0530

    Add files via upload

---
+++ TicTacToe/script.js
@@ -0,0 +1,80 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let boxes = document.querySelectorAll(".box");
+    let resetbtn = document.querySelector("#reset");
+    let msgcontainer = document.querySelector(".msg-container");
+    let msg = document.querySelector("#msg");
+    let turn0 = true;
+    let gameOver = false;
+
+    const winpattern = [
+        [0, 1, 2],
+        [0, 3, 6],
+        [0, 4, 8],
+        [1, 4, 7],
+        [2, 5, 8],
+        [2, 4, 6],
+        [3, 4, 5],
+        [6, 7, 8],
+    ];
+
+    const resetGame = () => {
+        turn0 = true; 
+        gameOver = false; 
+        for (let box of boxes) {
+            box.innerText = ""; 
+            box.disabled = false; 
+        }
+        msgcontainer.classList.add("hide");
+        console.log("Game has been reset.");
+    };
+
+    const disableboxes = () => {
+        for (let box of boxes) {
+            box.disabled = true;
+        }
+    };
+
+    const showWinner = (winner) => {
+        msg.innerText = `Congratulations, Winner is ${winner}`;
+        msgcontainer.classList.remove("hide");
+    };
+
+    const checkWinner = () => {
+        for (let pattern of winpattern) {
+            let pos1val = boxes[pattern[0]].innerText;
+            let pos2val = boxes[pattern[1]].innerText;
+            let pos3val = boxes[pattern[2]].innerText;
+
+            if (pos1val !== "" && pos2val !== "" && pos3val !== "") {
+                if (pos1val === pos2val && pos2val === pos3val) {
+                    console.log("Winner:", pos1val);
+                    showWinner(pos1val);
+                    disableboxes();
+                    gameOver = true;
+                    return;
+                }
+            }
+        }
+    };
+
+    boxes.forEach((box) => {
+        box.addEventListener("click", () => {
+            if (gameOver) return;
+
+            if (turn0) {
+                box.innerText = "O";
+                turn0 = false;
+            } else {
+                box.innerText = "X";
+                turn0 = true;
+            }
+            box.disabled = true;
+            checkWinner();
+        });
+    });
+
+    resetbtn.addEventListener("click", () => {
+        console.log("Reset Game button clicked");
+        resetGame();
+    });
+});
